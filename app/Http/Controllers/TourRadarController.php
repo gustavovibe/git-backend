1e2821de66248b5ecf4731a3c5d00cf42c809701
errbchile <errbchile@gmail.com>
2024-03-26 10:32:28 +0100

    refactoring formatTour

--- app/Http/Controllers/TourRadarController.php
+++ app/Http/Controllers/TourRadarController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Client\RequestException;
 use Illuminate\Support\Facades\Http;
 use App\Helpers\ApiResponse;
+use App\Helpers\FormatTour;
 use Illuminate\Support\Facades\Validator;
 
 class TourRadarController extends Controller
@@ -163,73 +164,19 @@ class TourRadarController extends Controller
         try {
             $response = Http::withHeaders($headers)->get($url);
             $tour = $response->json();
-            $tour = $this->formatTourData($tour);
+            $tour = FormatTour::formatTourData($tour);
             return $tour;
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
 
-    private function formatTourData($tour)
+    private function getPriceCategoriesByTour($accessToken, $tourId)
     {
-        $formatedTour = [];
-        $formatedTour['tour_id'] = $tour['tour_id'];
-        $formatedTour['tour_name'] = $tour['tour_name'];
-        $formatedTour['overview'] = $tour['description'];
-        $formatedTour['ratings'] = $this->getRatings($tour);
-        $formatedTour['reviews_count'] = $tour['reviews_count'];
-        $formatedTour['tour_length_days'] = $tour['tour_length_days'];
-        $formatedTour['max_group_size'] = $tour['max_group_size'];
-        $formatedTour['images'] = $this->getFormattedImages($tour);
-        $formatedTour['guiding_method'] = $this->getGuidingMethod($tour);
-        $formatedTour['tour_type'] = $this->getTourType($tour);
-        $formatedTour['tour_types'] = $tour['tour_types'];
-        $formatedTour['age_range_formatted'] = $this->getAgeRange($tour);
-        $formatedTour['age_range'] = $tour['age_range'];
-        $formatedTour['guide_languages'] = $this->getGuideLanguagesForTour($tour);
-        $formatedTour['start_city'] = $tour['start_city'];
-        $formatedTour['end_city'] = $tour['end_city'];
-        $formatedTour['destinations'] = $tour['destinations'];
-        $formatedTour['prices'] = $this->getFormattedPrice($tour);
-        $formatedTour['itinerary'] = $tour['itinerary'];
-        $formatedTour['services'] = $this->getServices($tour);
-        $formatedTour['operator'] = $tour['operator'];
-
-        return $formatedTour;
-    }
-
-    private function getFormattedPrice($tour)
-    {
-        $response = [];
-        $response['based_on'] = $tour['prices']['based_on'];
-        $response['price_total'] = $tour['prices']['price_total'];
-        $response['mandatory_addons'] = [];
-        $response['mandatory_addons'] = $tour['prices']['mandatory_addons'];
-        return $response;
-    }
-
-    private function getGuideLanguagesForTour($tour)
-    {
-        $response = [];
-        $taxonomy_languages = $this->getTaxonomyLanguages();
-        foreach ($tour['guide_languages'] as $languageId) {
-            foreach ($taxonomy_languages as $language) {
-                if ($language['id'] === $languageId) {
-                    array_push($response, $language);
-                    break;
-                }
-            }
-        }
-        return $response;
-    }
-
-    private function getTaxonomyLanguages()
-    {
-        $token = $this->getAccessToken();
-        $url = "https://api.sandbox.b2b.tourradar.com/v1/taxonomy/languages";
+        $url = "https://api.sandbox.b2b.tourradar.com/v1/tours/{$tourId}/prices";
         $headers = [
             'Accept' => 'application/json',
-            'Authorization' => 'Bearer ' . $token,
+            'Authorization' => 'Bearer ' . $accessToken,
         ];
 
         try {
@@ -240,81 +187,13 @@ class TourRadarController extends Controller
         }
     }
 
-    private function getAgeRange($tour)
+    private function getTaxonomyLanguages()
     {
-        $min = $tour['age_range']['strict']['min_age'];
-        $max = $tour['age_range']['strict']['max_age'];
-        return "{$min}-{$max}";
-    }
-
-    private function getTourType($tour)
-    {
-        $formatted = [];
-        foreach ($tour['tour_types'] as $tourType) {
-            if ($tourType['group_id'] === 1) {
-                array_push($formatted, $tourType);
-            }
-        }
-        return $formatted;
-    }
-
-    private function getServices($tour)
-    {
-        $included = [];
-        $excluded = [];
-        foreach ($tour['services'] as $serviceName => $serviceDetails) {
-            if (count($serviceDetails) === 0) {
-                continue;
-            }
-            foreach ($serviceDetails as $service) {
-                if ($service['is_included']) {
-                    $included[$serviceName] = [];
-                    array_push($included[$serviceName], $service);
-                } else {
-                    $excluded[$serviceName] = [];
-                    array_push($excluded[$serviceName], $service);
-                }
-            }
-        }
-        $response = [];
-        $response['included'] = $included;
-        $response['excluded'] = $excluded;
-        return $response;
-    }
-
-    private function getGuidingMethod($tour)
-    {
-        $formatted = [];
-        foreach ($tour['tour_types'] as $tourType) {
-            if ($tourType['group_id'] === 2) {
-                array_push($formatted, $tourType);
-            }
-        }
-        return $formatted;
-    }
-
-    private function getRatings($tour)
-    {
-        if (isset($tour['ratings']['overall'])) {
-            return $tour['ratings']['overall'];
-        } else {
-            return $tour['ratings']['operator'];
-        }
-    }
-
-    private function getFormattedImages($tour)
-    {
-        return array_map(function ($image) {
-            return $image['url'];
-        }, $tour['images']);
-    }
-
-    private function getPriceCategoriesByTour($accessToken, $tourId)
-    {
-        $url = "https://api.sandbox.b2b.tourradar.com/v1/tours/{$tourId}/prices";
+        $token = self::getAccessToken();
+        $url = "https://api.sandbox.b2b.tourradar.com/v1/taxonomy/languages";
         $headers = [
             'Accept' => 'application/json',
-            'Authorization' => 'Bearer ' . $accessToken,
+            'Authorization' => 'Bearer ' . $token,
         ];
 
         try {
