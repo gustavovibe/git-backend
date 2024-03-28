7e1aa5d5f33b1cc31ae37b26f1419dd9cb3af627
Developer Antonio Alonso <antonio.alonso8012@gmail.com>
2024-03-27 19:12:18 -0600

    filter city, country and natural destination

--- app/Http/Controllers/TourController.php
+++ app/Http/Controllers/TourController.php
@@ -11,21 +11,49 @@ class TourController extends Controller
 {
     public function index(Request $request)
     {
+        $perPage = 5;
+        $responseData = [];
 
-        $perPage = 10;
-
-        $q = $request->input('q');
-
-        if ($q) {
-            $paginatedData = Tour::with('cities', 'countries', 'natural_destination')->where('name', 'like', $q . '%')->paginate($perPage);
-        } else {
-            $paginatedData = Tour::with('cities', 'countries', 'natural_destination')->paginate($perPage);
+        if ($request->has('country')) {
+            $responseData['country'] = $this->filterByCountry($request->country, $perPage);
         }
-        $responseData = $paginatedData->toArray();
 
-        $responseData['data'] = TourResource::collection($paginatedData->items());
+        if ($request->has('city')) {
+            $responseData['city'] = $this->filterByCity($request->city, $perPage);
+        }
+
+        if ($request->has('natural_destination')) {
+            $responseData['natural_destinations'] = $this->filterByNaturalDestination($request->natural_destination, $perPage);
+        }
 
         return ApiResponse::success($responseData);
+    }
+
+    protected function filterByCountry($countryId, $perPage)
+    {
+        $query = Tour::whereHas('countries', function ($query) use ($countryId) {
+            $query->where('t_country_id', $countryId);
+        })->with('cities', 'natural_destination')->paginate($perPage);
+
+        return TourResource::collection($query->items());
+    }
+
+    protected function filterByCity($cityId, $perPage)
+    {
+        $query = Tour::whereHas('cities', function ($query) use ($cityId) {
+            $query->where('t_city_id', $cityId);
+        })->with('countries', 'natural_destination')->paginate($perPage);
+
+        return TourResource::collection($query->items());
+    }
+
+    protected function filterByNaturalDestination($naturalId, $perPage)
+    {
+        $query = Tour::whereHas('natural_destination', function ($query) use ($naturalId) {
+            $query->where('t_natural_id', $naturalId);
+        })->with('cities', 'countries')->paginate($perPage);
+
+        return TourResource::collection($query->items());
     }
 
 
