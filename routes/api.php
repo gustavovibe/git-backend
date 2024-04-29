fd9c735008ee074101af2d3ac70f07f90b7d9ed3
errbchile <errbchile@gmail.com>
2024-04-29 14:07:12 +0200

    new DuffelApiController

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\TourCountriesController;
 use App\Http\Controllers\TourNaturalDestinationController;
 use App\Http\Controllers\ProxyTourRadarController;
 use App\Http\Controllers\ProxyKiwiController;
+use App\Http\Controllers\DuffelApiController;
 
 Route::post('import-cities', [Citycontroller::class, 'import']);
 
@@ -57,3 +58,5 @@ Route::resource('tours', TourController::class);
 Route::resource('tour_countries', TourCountriesController::class);
 
 Route::resource('tour_natural_destinations', TourNaturalDestinationController::class);
+
+Route::get('duffel/offer-requests', [DuffelApiController::class, 'offerRequests']);
