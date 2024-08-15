76c5b411d8bf58b55366560933d08c7cc888099a
Gustavo Menchaca <gustavomenchaca.cecyt2@gmail.com>
2024-08-15 04:59:21 +0000

    get seats corrected

--- app/Http/Controllers/DuffelApiController.php
+++ app/Http/Controllers/DuffelApiController.php
@@ -134,7 +134,7 @@ class DuffelApiController extends Controller
             $headers = self::getHeaders();
 
             // Building url
-            $url = 'https://api.duffel.com/air/offers/' . $request->offerId;
+            $url = 'https://api.duffel.com/air/offers/' . $request->offer_id;
 
             // Make the request to the Duffel API
             $response = Http::withHeaders($headers)->get($url);
@@ -147,32 +147,31 @@ class DuffelApiController extends Controller
         }
     }
 
-        // api/duffel/get-offer-by-id
-        public function getSeats(Request $request)
-        {
-            // Validations
-            $validator = $this->validateParamsWhenOfferById($request);
-            if ($validator->fails()) {
-                return ApiResponse::error($validator->errors());
-            }
-    
-            try {
-                // Getting Headers
-                $headers = self::getHeaders();
-    
-                // Building url
-                $url = 'https://api.duffel.com/air/seat_maps' . $request->offerId;
-    
-                // Make the request to the Duffel API
-                $response = Http::withHeaders($headers)->get($url);
-    
-                // Return the response from the Duffel API
-                return $response->json();
-            } catch (\Exception $e) {
-                // Handle exceptions
-                return response()->json(['error' => $e->getMessage()], 500);
-            }
+    public function getSeats(Request $request)
+    {
+        // Validations
+        $validator = $this->validateParamsWhenOfferById($request);
+        if ($validator->fails()) {
+            return ApiResponse::error($validator->errors());
         }
+    
+        try {
+            // Getting Headers
+            $headers = self::getHeaders();
+    
+            // Building URL with the offer ID as a query parameter
+            $url = 'https://api.duffel.com/air/seat_maps?offer_id=' . $request->offerId;
+    
+            // Make the request to the Duffel API
+            $response = Http::withHeaders($headers)->get($url);
+    
+            // Return the response from the Duffel API
+            return $response->json();
+        } catch (\Exception $e) {
+            // Handle exceptions
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
 
     public static function createNewBooking($body)
     {
