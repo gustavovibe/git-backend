d243081b303fe64ab433398edfa4de4a6e780ab5
Adam_VA <adan_gonzalez@vibeadventures.com>
2024-08-17 13:11:47 -0700

    fix destinations filter

--- app/Filters/ToursFilters.php
+++ app/Filters/ToursFilters.php
@@ -5,6 +5,7 @@ namespace App\Filters;
 use App\Models\Order;
 use App\Models\Tour;
 use App\Models\Type;
+use App\Models\City;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -158,5 +159,98 @@ class ToursFilters
         }
 
         return $travel;
+    }
+    public function destinations(Request $r){
+        $destination= City::query();
+        $orderby=(int)$r->order;
+        $commission=explode(',',$r->commission);
+        $range=explode(',',$r->range);
+        $cities=$r->id_cities?explode(',',$r->id_cities):'';
+        $countries=$r->id_countries?explode(',',$r->id_countries):'';
+        $destinations=$r->id_destinations?explode(',',$r->id_destinations):'';
+        $minRange =(int) $range[0];
+        $maxRange =(int) $range[1];
+        $minCommission = (double)$commission[0];
+        $maxCommission = (double)$commission[1];
+        !$r->city_name?:$destination->where('city_name','like',"%{$r->city_name}%");
+        !$r->t_city_id?:$destination->where('t_city_id',$r->t_city_id);
+
+        $destination->whereHas('tours', function ($query) {
+            $query->whereHas('orders');
+        });
+
+        !$cities?:$destination->whereHas('tours', function($query) use ($cities) {
+            $query->whereIn('t_city_id', $cities);
+        });
+
+        if (!empty($destinations)) {
+            $destination->whereHas('tours', function($query) use ($destinations) {
+                $query->whereHas('tour', function($query) use ($destinations) {
+                    $query->whereHas('natural_destination', function($query) use ($destinations) {
+                        $query->whereIn('t_natural_id', $destinations);
+                    });
+                });
+            });
+        }
+
+
+        !$r->limit?:$destination->limit($r->limit);
+
+        if(in_array($orderby,[1,2])){
+            $orderby==1?$destination->orderBy('city_name', 'ASC'):$destination->orderBy('city_name', 'DESC');
+        }
+
+
+        $destination = $destination->withCount('tours')->get()->map(function($des) use($minCommission, $maxCommission){
+            $des->tour_ids = $des->tours->map(function($tour) {
+                return $tour->tour_id;
+            })->values()->toArray();
+
+            $orders = Order::whereIn('tour_id', $des->tour_ids) ->whereBetween('commission', [$minCommission, $maxCommission])->select('tour_id', 'paid', 'commission')->get();
+
+            $des->total_paid_commission = 0;
+            $des->total_paid = 0;
+
+            $commision_r = [];
+            foreach ($orders as $order) {
+                $des->total_paid_commission += $order->commission * (double)$order->paid;
+                $des->total_paid += (double)$order->paid;
+                if (!in_array(100 * $order->commission, $commision_r)) {
+                    $commision_r[] = 100 * $order->commission;
+                }
+            }
+
+            $des->commission_r = count($commision_r) ? implode(',', $commision_r) : '0';
+            $des->commission_a =$commision_r;
+            $commision_r = [];
+            $des->total_paid_commission = round($des->total_paid_commission, 2);
+            $des->total_paid = round($des->total_paid, 2);
+
+            unset($des->tour_ids);
+            unset($des->tours);
+            return $des;
+        })->filter(function($des) use ($minRange, $maxRange) {
+            return ($des->total_paid >= $minRange && $des->total_paid <= $maxRange);
+
+        })->values()->all();
+
+        $val_list=[
+            3=>'total_paid',
+            4=>'total_paid',
+            7=>'tours_count',
+            8=>'tours_count',
+        ];
+        if ( in_array($orderby,[3,4,5,6,7,8])) {
+            usort($destination, function ($a, $b) use($orderby,$val_list){
+                if (in_array($orderby, [3, 5, 7])) {
+                    return $a->{$val_list[$orderby]} <=> $b->{$val_list[$orderby]};
+                }
+                if (in_array($orderby, [4, 6, 8])) {
+                    return $b->{$val_list[$orderby]} <=> $a->{$val_list[$orderby]};
+                }
+            });
+        }
+
+        return $destination;
     }
 }
