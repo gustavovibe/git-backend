b240e95dd154d6ca60c1010b3bc002be40a20f8f
Antonio Alonso <antonio.alonso8012@gmail.com>
2024-08-27 03:12:11 -0400

    fix: update migration users history

--- database/migrations/2024_08_27_021046_create_users_history_table.php
+++ database/migrations/2024_08_27_021046_create_users_history_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersHistoryTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('users_history', function (Blueprint $table) {
@@ -22,11 +17,6 @@ class CreateUsersHistoryTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('users_history');
