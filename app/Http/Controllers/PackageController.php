0c0fd570819535f23429694364db989095b1e694
Gustavo Menchaca <gustavomenchaca.cecyt2@gmail.com>
2024-10-01 05:24:56 +0000

    stripe total price modified

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -42,7 +42,7 @@ class PackageController extends Controller
 
         $tour = Tour::find($tour_id);
 
-        $amount = $tour->price_total * 100;
+        $amount = $order->price_total;
 
         $url = $request->url;
 
