5435eeac4a9633f770a2a14995a37b632ed5c2ee
Adam_VA <adan_gonzalez@vibeadventures.com>
2024-10-23 07:28:21 -0700

    fix sumary post-get

--- routes/api.php
+++ routes/api.php
@@ -151,7 +151,7 @@ Route::get('boooking-pdf',[TourController::class,'pdfOrder']);
 
 Route::post('/add-enquiry', [EnquiryController::class, 'create']);
 
-Route::post('boooking-summary',[TourController::class,'bookingSummarySend']);
+Route::get('boooking-summary',[TourController::class,'bookingSummarySend']);
 
 Route::get('boooking-summary-pdf',[TourController::class,'bookingSummaryPdf']);
 
