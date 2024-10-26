2220fab33095c7caf7e9ff5c54deaee18a6f42be
Adam_VA <adan_gonzalez@vibeadventures.com>
2024-10-25 19:10:58 -0700

    email test images

--- resources/views/emails/enquery_client.blade.php
+++ resources/views/emails/enquery_client.blade.php
@@ -580,7 +580,7 @@
                                 {{-- opcion 1 --}}
                                 <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('images/face-icon.png'))) }}">
                                 {{-- <img src="https://hopeful-nobel.74-208-189-166.plesk.page/public/images/face-icon.png"> --}}
-                                <img src="{{ $message->embed(public_path('images/insta-icon.png')) }}">
+                              {{--   <img src="{{ $message->embed(public_path('images/insta-icon.png')) }}"> --}}
                                 {{-- <img src="https://hopeful-nobel.74-208-189-166.plesk.page/public/images/insta-icon.png"> --}}
                                 <img src="https://hopeful-nobel.74-208-189-166.plesk.page/public/images/youtube-icon.png">
                         </td>
