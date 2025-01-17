33fda592406a45d35ec4b92cfcecb294998ca2ec
Gustavo Menchaca <gustavomenchaca.cecyt2@gmail.com>
2025-01-17 03:51:49 +0000

    Non-static method App\Filters\ToursFilters::OrdersPrint() cannot be called statically

--- app/Filters/ToursFilters.php
+++ app/Filters/ToursFilters.php
@@ -428,15 +428,14 @@ class ToursFilters
         return $destination;
     }
 
-    public function OrdersPrint(Request $r){
-        $orders = Order::with(['flightTour', 'travelers', 'user'])->find($r->tour_id);
+    public static function OrdersPrint(Request $r){
+       $orders = Order::with(['flightTour', 'travelers', 'user'])->find($r->tour_id);
 
        $orders->days= Carbon::parse($orders->start)->diffInDays(Carbon::parse($orders->end));
         $orders->image=$orders->tour->main_image;
         $orders->reviews_count=$orders->tour->reviews_count;
         $orders->ratings_overall=$orders->tour->ratings_overall;
         unset($orders->tour);
-
         return $orders;
     }
 
