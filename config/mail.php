d68724197352366a2ef420fceaa66d3661aaff69
Gustavo Menchaca <gustavomenchaca.cecyt2@gmail.com>
2025-03-08 02:32:19 +0000

    tsl

--- config/mail.php
+++ config/mail.php
@@ -37,8 +37,8 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.zoho.com'),
-            'port' => env('MAIL_PORT', 465),
-            'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
+            'port' => env('MAIL_PORT', 587),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME', 'bookings@vibeadventures.com'),
             'password' => env('MAIL_PASSWORD', 'Adventures4ever'),
             'timeout' => null,
