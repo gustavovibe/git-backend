00baa6f44747e41b6ac94dcf15892c154383a981
Gustavo Menchaca <gustavomenchaca.cecyt2@gmail.com>
2025-05-09 06:07:10 +0000

    invoice adjusted to figma

--- resources/views/emails/invoice.blade.php
+++ resources/views/emails/invoice.blade.php
@@ -1,27 +1,27 @@
 <body>
-    <div style="padding: 15%">
+    <div style="padding: 4%">
 
         <table style="width: 100%">
             <tr>
                 <td>
                     <div>
-                        <h2 style="color: #7F91A8">INVOICE</h2>
+                        <h2 style="color: black">INVOICE</h2>
                         <h2 style="color: #7F91A8">{{ $data['charge_details']['receipt_number'] }}</h2>
                     </div>
                 </td>
                 <td style="text-align: right">
-                    <img style="width: 20%" src="https://vibeadventures.be/images/logo.png" alt="">
+                    <img style="width: 30%" src="https://vibeadventures.be/images/logo.png" alt="">
                 </td>
             </tr>
 
 
 
         </table>
-        <h2 style="text-align: center ;background-color: #FAE8E8; padding:1%;" >This invoice was already paid.</h2>
+        <h2 style="text-align: center; color: #650808; padding:1%;" >This invoice was already paid.</h2>
 
         <table style="width: 100%;">
             <tr>
-                <td style="width: 49.5%; border-style:solid;padding:3%;border-color:#F5F7F9;">
+                <td style="width: 48%; padding:3%; border: 1px solid #FAFAFA;">
                     <div>
                         <p><b>Supplier</b></p>
                         <p>Vibe Adventures, Inc.</p>
@@ -36,34 +36,39 @@
                         <p>{{ $data['charge_details']['billing_details']['address']['state'] }}</p>
                     </div>
                 </td>
-                <td style="width: 1%;"></td>
-                <td style="background-color:#F5F7F9; padding:3%">
-                    <div style="margin-bottom:20%; text-align:center;">
+                <td style="width: 4%;"></td>
+                <td style="padding:3%">
+                    <div style="margin-bottom:10%; text-align:center;">
                         <table>
                             <tr>
                                 <td> <p><b>Booking ID</b></p> </td>
+                                <td style="width: 3%;"></td>
                                 <td> <p>{{ $orders->booking_id }}</p></td>
                             </tr>
                             <tr>
                                 <td> <p><b>Type</b></p> </td>
-                                <td> <p>{{ $orders->flightTour->tour['type'] }}</p></td>
+                                <td style="width: 3%;"></td>
+                                <td> <p>Booking</p></td>
                             </tr>
                             <tr>
                                 <td> <p><b>Issue Date</b></p> </td>
-                                <td> <p>{{ date('Y-m-d',$data['balance_transaction']['created'] + (5 * 86400)) }}</p></td>
+                                <td style="width: 3%;"></td>
+                                <td> <p>{{ date('Y-m-d',$data['balance_transaction']['created']) }}</p></td>
                             </tr>
                             <tr>
                                 <td> <p><b>Due Date</b></p> </td>
-                                {{-- <td> <p>{{ date('Y-m-d',$data['payment_intent']['created'] + (5 * 86400)) }}</p></td> --}}
-                                <td> <p>{{ date('Y-m-d',$data['balance_transaction']['created'] + (5 * 86400)) }}</p></td>
+                                <td style="width: 3%;"></td>
+                                <td> <p>{{ date('Y-m-d',$data['balance_transaction']['created']) }}</p></td>
                             </tr>
                             <tr>
                                 <td> <p><b>Taxable Date</b></p> </td>
-                                <td> <p>{{ date('Y-m-d',$data['balance_transaction']['created'] + (5 * 86400)) }}</p></td>
+                                <td style="width: 3%;"></td>
+                                <td> <p>{{ date('Y-m-d',$data['balance_transaction']['created']) }}</p></td>
                             </tr>
                             <tr>
                                 <td> <p><b>Currency</b></p> </td>
-                                <td> <p>{{ $data['payment_intent']['currency'] }}</p></td>
+                                <td style="width: 3%;"></td>
+                                <td> <p>{{ strtoupper($data['payment_intent']['currency']) }}</p></td>
                             </tr>
                         </table>
                     </div>
@@ -72,75 +77,47 @@
         </table>
         <br>
         <table style="width: 100%;border-collapse: collapse; text-align:center">
-            <tr style="background: #F5F7F9;">
+            <tr>
                 <th style="width: 10%;padding:1%"> <p>Item</p></th>
-                <th style="width: 30%">Description</th>
-                <th style="width: 10%">Vat</th>
+                <th style="width: 50%">Description</th>
                 <th style="width: 15%">Unit Price</th>
                 <th style="width: 10%"> Qty</th>
                 <th style="width: 15%">Price inc. VAT</th>
             </tr>
             <tr>
                 <td><p>Trip</p></td>
-                <td><p>{{ $orders->tour_name }}</p></td>
-                <td>
-                    <div>
-                        @if($values['adults'] > 0)
-                        <p>{{ $values['tax'] }}</p>
-                        @endif
-                        @if($values['children'] > 0)
-                        <p>{{ $values['tax'] }}</p>
-                        @endif
-                        @if($values['travelers'] > 0)
-                        <p>{{ $values['tax'] }}</p>
-                        @endif
-
-                    </div>
+                <td style="border-right: 1px dotted black">    
+                    <p>
+                        {{ $orders->tour_name }} organized adventure
+                        ({{ \Carbon\Carbon::parse($orders->arrival)->format('M d, Y') }} - {{ \Carbon\Carbon::parse($orders->end)->format('M d, Y') }}) for {{$orders->travelers_number}} travelers:
+                    </p><br>
+                    <ul>
+                    <li style="color: #9ca3af;font-family: 'Interstate Light Cond', sans-serif; font-size: 12px;">
+						@foreach ($orders['passengers'] as $acc)
+                            <span style="color: #82CF45;">
+                                {{ $acc['passengers'] }}
+                            </span>
+                                × {{ $acc['name'] }}
+                        @endforeach
+                    </li>
+                    @if ($orders->attempt->duffel_res['data']['slices'])
+                    <li>
+                    flights ( {{$orders->attempt->duffel_res['data']['slices'][0]['segments'][0]['origin']['iata_code']}} 
+                    - {{$orders->attempt->duffel_res['data']['slices'][0]['segments'][0]['destination']['iata_code']}} 
+                    on {{ \Carbon\Carbon::parse($orders->attempt->duffel_res['data']['slices'][0]['segments'][0]['departing_at'])->format('D, d/m') }}
+                    ; {{$orders->attempt->duffel_res['data']['slices'][1]['segments'][0]['origin']['iata_code']}} 
+                    - {{$orders->attempt->duffel_res['data']['slices'][1]['segments'][0]['destination']['iata_code']}} 
+                    on {{ \Carbon\Carbon::parse($orders->attempt->duffel_res['data']['slices'][1]['segments'][0]['departing_at'])->format('D, d/m') }}) 
+                    </li>
+                    @endif
                 </td>
-                <td>
-                    <div>
-                        @if($values['adults'] > 0)
-                        <p>US$ {{ $values['unitPrice'] }}</p>
-                        @endif
-                        @if($values['children'] > 0)
-                        <p>US$ {{ $values['unitPrice'] }}</p>
-                        @endif
-                        @if($values['travelers'] > 0)
-                        <p>US$ {{ $values['unitPrice'] }}</p>
-                        @endif
-
-                    </div>
-                </td>
-                <td>
-                    <div>
-                        @if($values['adults'] > 0)
-                        <p>{{ $values['adults'] }} <b style="color: #82CF45">adult(s)</b></p>
-                        @endif
-                        @if($values['children'] > 0)
-                        <p>{{ $values['children'] }} <b style="color: #82CF45">child(s)</b></p>
-                        @endif
-                        @if($values['travelers'] > 0)
-                        <p>{{ $values['travelers'] }} <b style="color: #82CF45">traveler(s)</b></p>
-                        @endif
-                    </div>
-                </td>
-                <td>
-                    <div>
-                        @if($values['adults'] > 0)
-                        <p>US$ {{ $values['adults'] *  ($values['unitPrice'])  }} <b style="color: #82CF45">adult(s)</b></p>
-                        @endif
-                        @if($values['children'] > 0)
-                        <p>US$ {{ $values['children'] *  ($values['unitPrice'])  }} <b style="color: #82CF45">child(s)</b></p>
-                        @endif
-                        @if($values['travelers'] > 0)
-                        <p>US$ {{ $values['travelers'] *   ($values['unitPrice'])  }} <b style="color: #82CF45">infant(s)</b></p>
-                        @endif
-                    </div>
-                </td>
+                <td><p>{{ 'US $'.$orders->paid }}</p></td>
+                <td><p>1</p></td>
+                <td><p>{{ 'US $'.$orders->paid }}</p></td>
             </tr>
         </table>
         <br>
-        <hr  style="border-top: 1px solid #82CF45;">
+        <hr  style="border-bottom: 1px solid #82CF45;">
         <br>
         <div>
 
