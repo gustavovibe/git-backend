e8892c48c41f5c38fe2ce518fd8f994563776ce0
Gustavo Menchaca <gustavomenchaca.cecyt2@gmail.com>
2025-05-10 05:21:29 +0000

    styles part 2

--- resources/views/emails/invoice.blade.php
+++ resources/views/emails/invoice.blade.php
@@ -14,16 +14,16 @@
             </tr>
         </table>
 
-        <h2 style="text-align: center; color: #650808; padding:1%;font-size:12px">This invoice was already paid.</h2>
+        <h2 style="text-align: center; font-family: Canaro, sans-serif; color: #650808; padding:1%;font-size:12px">This invoice was already paid.</h2>
 
-        <table style="width: 100%;">
+        <table style="width: 100%;margin-bottom:20px">
             <tr>
                 <td style="width: 48%; padding:3%; border: 1px solid #FAFAFA;">
                     <div style="font-family: Canaro, sans-serif;font-size:10px">
-                        <span><b>Supplier</b></span>
-                        <span>Vibe Adventures, Inc.</span>
-                        <span>300 Delaware Ave,Ste 210 #549</span>
-                        <span>Wilmington, 1981</span>
+                        <span><b>Supplier</b></span><br>
+                        <span>Vibe Adventures, Inc.</span><br>
+                        <span>300 Delaware Ave,Ste 210 #549</span><br>
+                        <span>Wilmington, 1981</span><br>
                         <span>US</span>
                         <br>
                         <span><b>Customer</b></span>
@@ -33,38 +33,38 @@
                         <span>{{ $data['charge_details']['billing_details']['address']['state'] }}</span>
                     </div>
                 </td>
-                <td style="width: 4%;"></td>
+                <td style="width: 30px;"></td>
                 <td style="padding:3%">
                     <div style="margin-bottom:10%; text-align:center;">
                         <table style="font-family: Canaro, sans-serif;font-size:10px">
                             <tr>
                                 <td> <span><b>Booking ID</b></span> </td>
-                                <td style="width: 3%;"></td>
+                                <td style="width: 10px;"></td>
                                 <td> <span>{{ $orders->booking_id }}</span></td>
                             </tr>
                             <tr>
                                 <td> <span><b>Type</b></span> </td>
-                                <td style="width: 3%;"></td>
+                                <td style="width: 10px;"></td>
                                 <td> <span>Booking</span></td>
                             </tr>
                             <tr>
                                 <td> <span><b>Issue Date</b></span> </td>
-                                <td style="width: 3%;"></td>
+                                <td style="width: 10px;"></td>
                                 <td> <span>{{ date('Y-m-d',$data['balance_transaction']['created']) }}</span></td>
                             </tr>
                             <tr>
                                 <td> <span><b>Due Date</b></span> </td>
-                                <td style="width: 3%;"></td>
+                                <td style="width: 10px;"></td>
                                 <td> <span>{{ date('Y-m-d',$data['balance_transaction']['created']) }}</span></td>
                             </tr>
                             <tr>
                                 <td> <span><b>Taxable Date</b></span> </td>
-                                <td style="width: 3%;"></td>
+                                <td style="width: 10px;"></td>
                                 <td> <span>{{ date('Y-m-d',$data['balance_transaction']['created']) }}</span></td>
                             </tr>
                             <tr>
                                 <td> <span><b>Currency</b></span> </td>
-                                <td style="width: 3%;"></td>
+                                <td style="width: 10px;"></td>
                                 <td> <span>{{ strtoupper($data['payment_intent']['currency']) }}</span></td>
                             </tr>
                         </table>
@@ -73,17 +73,17 @@
             </tr>
         </table>
         <br>
-        <table style="width: 100%;border-bottom: 1px solid #82CF45; text-align:left;font-family: Canaro, sans-serif;font-size:10px">
-            <tr>
+        <table style="width: 100%;border-bottom: 1px solid #82CF45; font-family: Canaro, sans-serif;font-size:10px">
+            <tr style="text-align:left">
                 <th style="width: 10%;padding:1%"> <span>Item</span></th>
                 <th style="width: 50%">Description</th>
                 <th style="width: 15%">Unit Price</th>
                 <th style="width: 10%"> Qty</th>
                 <th style="width: 15%">Price inc. VAT</th>
             </tr>
-            <tr>
+            <tr style="text-align:left">
                 <td><span>Trip</span></td>
-                <td style="border-right: 1px dotted black;font-">    
+                <td style="border-right: 1px dotted black;">    
                     <span>
                     <span style="color: #82CF45;">{{ $orders->tour_name }} organized adventure</span>
                         ({{ \Carbon\Carbon::parse($orders->arrival)->format('M d, Y') }} - {{ \Carbon\Carbon::parse($orders->end)->format('M d, Y') }}) for {{$orders->travelers_number}} traveler(s):
@@ -114,7 +114,7 @@
         </table>
         <br>
         <div>
-            <table style="text-align: right; width:100%" >
+            <table style="text-align: right; font-family: Canaro, sans-serif;font-size:12px width:100%" >
                 <tr>
                     <td style="width: 70%"></td>
                     <th>Subtotal</th>
@@ -136,12 +136,12 @@
     <table style="text-align:left">
         <tr>
             <td>
-            <p style="font-family: Canaro, sans-serif;font-size:10px">* This is a simplified invoice. If you require an electronic invoice in CFDI format, please <a href="https://vibeadventures.com/contact" style="color: #82CF45;" >contact us</a>.</span>
+            <span style="font-family: Canaro, sans-serif;font-size:10px">* This is a simplified invoice. If you require an electronic invoice in CFDI format, please <a href="https://vibeadventures.com/contact" style="color: #82CF45;" >contact us</a>.</span>
             </td>
         </tr>
         <tr>
             <td>
-            <p style="font-family: Canaro, sans-serif;font-size:10px">For more information visit:<a href="https://vibeadventures.com">https://vibeadventures.com</a></span>
+            <span style="font-family: Canaro, sans-serif;font-size:10px">For more information visit:<a href="https://vibeadventures.com">https://vibeadventures.com</a></span>
             </td>
         </tr>
     </table>
